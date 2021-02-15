4cba863b9b420df36877153aca439be105671d31
Richard Cunha <decunha@me.com>
2021-02-15 15:23:53 -0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 import { errorHandler, notFoundHandler } from "./middleware";
 import apiRouter from "./routes";
@@ -6,6 +7,8 @@ import apiRouter from "./routes";
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/books", apiRouter);
 
